1a5aea78ae21a3f8c15c4dd0369a3ef95b174aed
icanblink <bogdan.barna@3pillarglobal.com>
2015-03-04 14:55:22 +0200

    added custom filter, reduce functionality

--- fun.py
+++ fun.py
@@ -54,6 +54,33 @@ def my_map(func, sequence):
     return [func(item) for item in sequence]
 
 
+def my_filter(func, sequence):
+    """Returns a list after applying the filter to each item of sequence."""
+    return [item for item in sequence if func(item)]
+
+
+def my_reduce(func, iterable, initializer=None):
+    """
+    Applies the function to prev element (or initializer for the first time)
+    from left to right using the previous value as the first parameter of
+    the function. Return the final result.
+    """
+    items = iter(iterable)
+
+    if initializer is None:
+        initializer = next(items)
+
+    prev = initializer
+    for i in items:
+        prev = func(prev, i)
+
+    return prev
+
+
+def sum_fancy_numbers(stop):
+    """Sums up all the numbers n that are lower than the input and have the property 3 | n * n - 1."""
+    return my_reduce(lambda a, b: a+b, my_filter(lambda x: True if (x * x - 1) % 3 == 0 and x > 2 else False, range(1, stop)))
+
 if __name__ == "__main__":
     num = int(raw_input("Number: "))
     result = "is prime" if check_prime(num) else "is not prime"
@@ -64,7 +91,11 @@ if __name__ == "__main__":
     print primes
 
     fib_no = int(raw_input("Fibonacci numbers: "))
-    print fibonacci_iterative(fib_no)
-    print fibonacci_recursive(fib_no)
+    print "iterative: ", fibonacci_iterative(fib_no)
+    print "recursive: ", fibonacci_recursive(fib_no)
 
-    print my_map(str.upper, ['test', 'test1', 'test2'])
+    print my_map(str.upper, ["test", "test1", "test2"])
+    print my_filter("test".__eq__, ["test", "test1", "test2"])
+    print my_reduce(lambda a, b: a+b, [1, 1, 1, 1])
+
+    print sum_fancy_numbers(5)
