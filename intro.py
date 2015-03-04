815ba80da12bdf5d3dd962122c325f567f04099d
icanblink <bogdan.barna@3pillarglobal.com>
2015-03-04 10:32:48 +0200

    refactored functions from intro

--- intro.py
+++ intro.py
@@ -1,10 +1,12 @@
-def intro1():
+def reverse_input_name():
+    """Read a name and print its reverse."""
     name = raw_input("Name: ")
     reversed_name = name[::-1]
     print "Your reverse name is: ", reversed_name
 
 
-def intro2():
+def check_number_palindrome():
+    """Read a number and print if its palindrome or not."""
     num = int(raw_input("Number: "))
     n = num
     reversed_num = 0
@@ -13,19 +15,22 @@ def intro2():
         n /= 10
 
     result = "is" if num == reversed_num else "is not"
-    result += " a palindrome"
-    print result
+    print "%s a palindrome" % result
 
 
-def intro3():
+def palindromes():
+    """Read a number and prints all lower palindromes."""
     n = int(raw_input("Give upper limit: "))
 
+    pals = list()
     for i in range(0, n):
         i_str = str(i)
         if i < 10 or i_str == i_str[::-1]:
-            print i,
+            pals.append(i_str)
+
+    print ", ".join(pals)
 
 if __name__ == '__main__':
-    intro1()
-    intro2()
-    intro3()
+    reverse_input_name()
+    check_number_palindrome()
+    palindromes()
